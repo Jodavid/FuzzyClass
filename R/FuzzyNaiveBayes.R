b1f24fb3b7e2689c3da91e66571c4bb8613ab2ae
jodavid <jodavid@protonmail.com>
2022-02-19 17:26:26 -0300

    atualizacao do Fuzzy Naive Bayes

--- R/FuzzyNaiveBayes.R
+++ R/FuzzyNaiveBayes.R
@@ -5,7 +5,6 @@
 #'
 #' @param train matrix or data frame of training set cases.
 #' @param cl factor of true classifications of training set
-#' @param cores  how many cores of the computer do you want to use (default = 2)
 #' @param fuzzy boolean variable to use the membership function
 #' @param m is M/N, where M is the number of classes and N is the number of train lines
 #' @param Pi is 1/M, where M is the number of classes
@@ -18,35 +17,35 @@
 #' @examples
 #'
 #' set.seed(1) # determining a seed
-#' data(iris)
+#' data(HouseVotes84, package = "mlbench")
 #'
 #' # Splitting into Training and Testing
-#' split <- caTools::sample.split(t(iris[, 1]), SplitRatio = 0.7)
-#' Train <- subset(iris, split == "TRUE")
-#' Test <- subset(iris, split == "FALSE")
+#' split <- caTools::sample.split(t(HouseVotes84[, 1]), SplitRatio = 0.7)
+#' Train <- subset(HouseVotes84, split == "TRUE")
+#' Test <- subset(HouseVotes84, split == "FALSE")
 #' # ----------------
 #' # matrix or data frame of test set cases.
 #' # A vector will be interpreted as a row vector for a single case.
-#' test <- Test[, -5]
+#' test <- Test[, -1]
 #' fit_FNB <- FuzzyNaiveBayes(
-#'   train = Train[, -5],
-#'   cl = Train[, 5], cores = 2
+#'   train = Train[, -1],
+#'   cl = Train[, 1]
 #' )
 #'
 #' pred_FNB <- predict(fit_FNB, test)
 #'
 #' head(pred_FNB)
-#' head(Test[, 5])
+#' head(Test[, 1])
 #'
 #' @importFrom stats model.extract na.pass sd terms predict
 #'
 #' @export
-FuzzyNaiveBayes <- function(train, cl, cores = 2, fuzzy = T, m = NULL, Pi = NULL) {
+FuzzyNaiveBayes <- function(train, cl, fuzzy = T, m = NULL, Pi = NULL) {
   UseMethod("FuzzyNaiveBayes")
 }
 
 #' @export
-FuzzyNaiveBayes.default <- function(train, cl, cores = 2, fuzzy = T, m = NULL, Pi = NULL) {
+FuzzyNaiveBayes.default <- function(train, cl, fuzzy = T, m = NULL, Pi = NULL) {
 
   # --------------------------------------------------------
   # Estimating class parameters
@@ -57,33 +56,41 @@ FuzzyNaiveBayes.default <- function(train, cl, cores = 2, fuzzy = T, m = NULL, P
   }
   dados <- train # training data matrix
   M <- c(unlist(cl)) # true classes
-  M <- factor(M, labels = unique(M))
-
+  if(!is.factor(M))  M <- factor(M, levels = unique(M))
+  # --
+  res <- sapply(1:cols, function(i) {is.factor(train[,i])})
+  if(sum(res) != cols){ stop("All variables must be categorical (factor).") }
+  # --
   N <- nrow(dados)
   # --------------------------------------------------------
 
   # --------------------------------------------------------
-  # Estimating Gamma Parameters
+  # Estimating Parameters
   if(is.null(m) & is.null(Pi)){ m <- length(unique(cl))/nrow(dados); Pi <-  1/m}
   # ---
   # Lista: variavel x classes
+  parametersC <- lapply(1:length(unique(M)), function(i) {
+    lapply(1:cols, function(j) {
+      SubSet <- dados[M == unique(M)[i], j]
+      #SubSet <- na.omit(SubSet)
+      param <- (table(SubSet) + m*pi)/(sum(table(SubSet)) + m)
+      return(param)
+    })
+  })
+
   model <- e1071::naiveBayes(x = dados, y = cl)
-  parametersC <- model$tables
+  #parametersC <- model$tables
   # --------------------------------------------------------
 
   #--------------------------------------------------------
-  Sturges <- Sturges(dados, M);
-  Comprim_Intervalo <- Comprim_Intervalo(dados, M, Sturges);
-  minimos <- minimos(dados, M, cols);
-  Freq <- Freq(dados, M, Comprim_Intervalo, Sturges, minimos, cols);
-  Pertinencia <- Pertinencia(Freq, dados, M);
-  #------
-  # --------------------------------------------------------
-  # Cria a funcao de pertinencia para cada classe, a partir das frequencias relativas
   Pertinencia <- lapply(1:length(unique(M)), function(i) {
-    Freq[[i]] / nrow(dados[M == unique(M)[i], ])
+    lapply(1:cols, function(j) {
+      SubSet <- dados[M == unique(M)[i], j]
+      param <- (table(SubSet))/(sum(table(SubSet)))
+      return(param)
+    })
   })
-  # ------
+  #------
   # Probabilidade a priori das classes - consideradas iguais
   pk <- rep(1 / length(unique(M)), length(unique(M)))
   # -------------------------------------------------------
@@ -92,13 +99,9 @@ FuzzyNaiveBayes.default <- function(train, cl, cores = 2, fuzzy = T, m = NULL, P
   # -------------------------------------------------------
   structure(list(
     parametersC = parametersC,
-    minimos = minimos,
     cols = cols,
     M = M,
-    cores = cores,
-    Comprim_Intervalo = Comprim_Intervalo,
     Pertinencia = Pertinencia,
-    Sturges = Sturges,
     pk = pk,
     fuzzy = fuzzy,
     model = model
@@ -134,13 +137,9 @@ predict.FuzzyNaiveBayes <- function(object,
   test <- as.data.frame(newdata)
   # --------------------------------------------------------
   parametersC <- object$parametersC
-  minimos <- object$minimos
   cols <- object$cols
   M <- object$M
-  cores <- object$cores
-  Comprim_Intervalo <- object$Comprim_Intervalo
   Pertinencia <- object$Pertinencia
-  Sturges <- object$Sturges
   pk <- object$pk
   fuzzy <- object$fuzzy
   model = object$model
@@ -161,69 +160,57 @@ predict.FuzzyNaiveBayes <- function(object,
 
 
   if (fuzzy == T) {
-  P <- lapply(1:length(unique(M)), function(i) {
+      N <- nrow(test)
+      # --
+      P <- lapply(1:length(unique(M)), function(i) {
         densidades <- sapply(1:cols, function(j) {
-          parametersC[[j]][i,2]*test[,j]
+          # --
+          sapply( 1:N, function(v){
+            if(is.na(test[v,j])){
+              return(1e-5)
+            }else{
+              pos <- which(names(parametersC[[i]][[j]])==(test[v,j]))
+              return(parametersC[[i]][[j]][pos])
+            }
+          })
+          # --
         })
         densidades <- apply(densidades, 1, prod)
+        # --
         p <- pk[[i]] * densidades
-
         # ---
         return(p)
       })
-
-  N_test <- nrow(test)
-  # --------------
-  # Defining how many CPU cores to use
-  core <- parallel::makePSOCKcluster(cores)
-  doParallel::registerDoParallel(core)
-  # --------------
-  # loop start
-  R_M_obs <- foreach::foreach(h = 1:N_test, .combine = rbind) %dopar% {
-
-    # ------------
-    x <- test[h, ]
-    # ------------
-    ACHOU_t <- c()
-    ACHOU <- 0
-
-    # ---------------
-    for (classe in 1:length(unique(M))) {
       # --
+
       # --
-      for (coluna in 1:cols) { # coluna da classe
-        for (linhaF in 1:Sturges[[classe]]) { # linha da classe
-          faixa <- minimos[[classe]][coluna] + Comprim_Intervalo[[classe]][coluna] # faixa de frequencia inicial
-          if (x[coluna] < faixa) { # ve se valor da classe pertence aaquela faixa
-            ACHOU[coluna] <- Pertinencia[[classe]][linhaF, coluna] # acumula valor na faixa de frequencia e interrompe este ultimo for
-            break
-          }
-          if (linhaF == Sturges[[classe]]) {
-            ACHOU[coluna] <- Pertinencia[[classe]][linhaF, coluna]
-            break
-          }
-          faixa <- faixa + Comprim_Intervalo[[classe]][coluna] # troca de faixa -> proxima
-        }
+      P_fuzzy <- lapply(1:length(unique(M)), function(i) {
+        densidades <- sapply(1:cols, function(j) {
+          # --
+          sapply( 1:N, function(v){
+            if(is.na(test[v,j])){
+              return(1e-5)
+            }else{
+              pos <- which(names(Pertinencia[[i]][[j]])==(test[v,j]))
+              return(Pertinencia[[i]][[j]][pos])
+            }
+          })
+          # --
+        })
+        densidades <- apply(densidades, 1, prod)
+        # --
+        p <- pk[[i]] * densidades
+        # ---
+        return(p)
+      })
+      # --
+
+      # --
+      R_M_obs <- array(NA,dim = c(length(P_fuzzy[[1]]),length(unique(M))))
+      for( i in 1:length(P)){
+        R_M_obs[,i] <- P[[i]] * P_fuzzy[[i]]
       }
-      # ---
-      ACHOU_t <- rbind(ACHOU_t, ACHOU) # Classes são as linhas
-      # ---
-    }
-    # -----
-    row.names(ACHOU_t) <- unique(M)
-    # --------------------------------------------------------
-    ACHOU_t <- apply(ACHOU_t, 1, prod)
-
-    f <- sapply(1:length(unique(M)), function(i) {
-      P[[i]][h] * ACHOU_t[i]
-    })
-  # -------------------------------------------------------
-
-    return(f)
-  }
-  # ------------
-  # -------------------------
-  parallel::stopCluster(core)
+      # --
   # ---------
   if (type == "class") {
     # -------------------------
@@ -235,7 +222,7 @@ predict.FuzzyNaiveBayes <- function(object,
     # -------------------------
     Infpos <- which(R_M_obs==Inf)
     R_M_obs[Infpos] <- .Machine$integer.max;
-    R_M_obs <- matrix(unlist(R_M_obs),ncol = cols)
+    R_M_obs <- matrix(unlist(R_M_obs),ncol = length(unique(M)))
     R_M_obs <- R_M_obs/rowSums(R_M_obs,na.rm = T)
     # ----------
     colnames(R_M_obs) <- unique(M)
@@ -245,6 +232,5 @@ predict.FuzzyNaiveBayes <- function(object,
   }
 
   return(R_M_obs)
-
 
 }
